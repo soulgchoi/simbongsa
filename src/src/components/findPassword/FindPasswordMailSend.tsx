11480ff93d1862b5d491be01641b18a7abe5d777
LimHaksu <neroren2@gmail.com>
2020-01-28 18:08:25 +0900

    버튼과 인풋 부분을 컴포넌트로 바꿈, (error 부분은 내일 작업)

---
+++ src/src/components/findPassword/FindPasswordMailSend.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+import { Link } from "react-router-dom";
+// import "assets/css/style.scss";
+// import "assets/css/user.scss";
+import "assets/mycss/components.scss";
+
+interface IProps {
+  location: {
+    state: {
+      email: "string";
+    };
+  };
+}
+interface IState {}
+
+class FindPasswordMailSend extends React.Component<
+  IProps,
+  IState,
+  RouteComponentProps
+> {
+  state = {
+    email: null
+  };
+  componentWillReceiveProps() {
+    console.log(this.props.location);
+    this.setState({ email: this.props.location.state.email });
+  }
+  componentDidMount() {
+    console.log(this.props.location);
+    this.setState({ email: this.props.location.state.email });
+  }
+  render() {
+    return (
+      <div className="user" id="login">
+        <div className="wrapC">
+          <h1 className="title">비밀번호 재설정 메일 전송</h1>
+          <div className="input-with-label">
+            <h3>
+              비밀번호 재설정 메일을 다시 보냈습니다. 메일함을 확인해 주세요.
+            </h3>
+          </div>
+          <Link
+            to={{
+              pathname: "/mailresend",
+              state: { email: this.state.email }
+            }}
+            className="btn--back"
+          >
+            <button className="btn btn--back btn--join">
+              메일이 도착하지 않았나요?
+            </button>
+          </Link>
+          <Link to={"/"} className="btn--back">
+            <button className="btn btn--back btn--join">로그인 화면으로</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FindPasswordMailSend;
