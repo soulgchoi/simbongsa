dfc8e2cb80f9bab80f3c971506480b0d8472d569
박정환 <pjh5929@naver.com>
2020-01-29 17:18:01 +0900

    login, logout

--- back_end/A205/src/main/java/com/a205/dto/MemberException.java
+++ back_end/A205/src/main/java/com/a205/dto/MemberException.java
@@ -7,7 +7,7 @@ public class MemberException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public MemberException() {
-		super("사원 정보 처리 중 오류 발생");
+		super("회원 정보 처리 중 오류 발생");
 	}
 
 	public MemberException(String msg) {
