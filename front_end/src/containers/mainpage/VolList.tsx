32e8e25a5dae4d284eefd1f19062552d1a8ec23c
Solji Choi <bssj9307@gmail.com>
2020-01-29 20:01:56 +0900

    오류 해결중

--- front_end/src/containers/mainpage/VolList.tsx
+++ front_end/src/containers/mainpage/VolList.tsx
@@ -5,32 +5,32 @@ import Vol from './Vol';
 interface Props {}
 
 interface State {
-    volunteers: Array<object>
+    volunteers: object;
 }
 
 class VolList extends Component<Props, State> {
     state = {
-        volunteers: []
+        volunteers: {}
     }
     componentWillMount(){
         axios.get('http://localhost:3001/volunteer')
         .then(response => {
             console.log(response.data)
-            this.setState({volunteers: response.data})
+            this.setState({volunteers: JSON.parse(response.data.toString())})
             console.log(this.state.volunteers)
-        })
+            })
         .catch(function(error) {
             console.log(error);
         })
     }
     render() {
-        const volunteers = this.state.volunteers.map( (volunteer, i) => {
-            return (
-                <div>
-                    <Vol volunteer={volunteer} key={i}/>
-                </div>
-            )
-        })
+        // const volunteers = this.state.volunteers.map( (volunteer, i) => {
+        //     return (
+        //         <div>
+        //             <Vol volunteer={volunteer} key={i}/>
+        //         </div>
+        //     )
+        // })
         return (
             <div>
                 {this.state.volunteers}
