0cceec87c98a233eec27a878b5dffbeb8fd0cc1e
min1378 <qwes123@naver.com>
2020-02-03 14:20:10 +0900

    create redux calendar

--- front_end/src/redux/modules/index.ts
+++ front_end/src/redux/modules/index.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import user from "./user";
-
+import calendar from "./calendar"
 import { penderReducer } from "redux-pender";
 
 export default combineReducers({
+  calendar,
   auth,
   user,
   pender: penderReducer
