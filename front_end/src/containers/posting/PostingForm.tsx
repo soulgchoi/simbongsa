ac6e5ae3395f9e3931bd3a1d89db90314c9d842d
Solji Choi <bssj9307@gmail.com>
2020-02-04 16:46:01 +0900

    포스팅에 이미지 업로드 추가, css 작업중 S02P13A205-24

--- front_end/src/containers/posting/PostingForm.tsx
+++ front_end/src/containers/posting/PostingForm.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import axios from 'axios'
-// import ImageUploadForm from 'containers/posting/ImageUploadForm'
+
+import 'assets/mycss/posting.scss'
+import 'assets/mycss/components.scss'
+import 'assets/mycss/fileinput.scss'
+
+import 'assets/css/style.scss'
 
 
 interface IProps {
@@ -10,7 +15,8 @@ interface IProps {
 class PostingForm extends React.Component<IProps, {}> {
     state = {
         content: "",
-        // date: "",
+        selectedFile: new File([""], "", {type: ""}),
+        imagePreview: ""
     }
 
     handleChange = (e: any) => {
@@ -19,31 +25,70 @@ class PostingForm extends React.Component<IProps, {}> {
         })
     }
 
+    handleFileChange = (e: any) => {
+        this.setState({
+            selectedFile: e.target.files[0]
+        })
+
+        let reader = new FileReader();
+        reader.onloadend = () => {
+            this.setState({
+                imagePreview: reader.result as string
+            })
+        }
+
+        reader.readAsDataURL(e.target.files[0])
+    }
+
     handleSubmit = (e: any) => {
         e.preventDefault();
 
-        axios.post("http://localhost:3001/post", {
-            "p_content": this.state.content
-        })
+        const fd = new FormData();
+        fd.append("image", this.state.selectedFile);
+        fd.set("data", this.state.content)
+        axios.post("http://localhost:3002/post", fd)
         .then(res => {
             console.log(res)
+            console.log(fd.get("image"))
+            console.log(fd.get("data"))
         })
 
     }
 
     render() {
+        let $imagePreview = (
+            <div></div>
+        );
+        if (this.state.imagePreview) {
+            $imagePreview = (
+                <div>
+                    <img src={this.state.imagePreview} alt="uploaded image" width="200" />
+                </div>
+            )
+        }
+
         return (
+            <div className="wrapC">
             <form
                 onSubmit={this.handleSubmit}
                 className="posting-form">
                 <input
+                    className="posting"
                     type="textarea"
                     name="content"
                     placeholder="내용을 입력하세요."
                     onChange={this.handleChange} />
-                <button type="submit">게시글 등록하기</button>
+                <input
+                    type="file"
+                    id="file"
+                    onChange={this.handleFileChange}
+                />
+                <label htmlFor="file" className="btn-1">이미지 업로드</label>
+                {$imagePreview}
+                <button className="my--btn" type="submit" onClick={this.handleSubmit}>게시글 등록하기</button>
+                
              </form>
-
+             </div>
         );
     }
 };
