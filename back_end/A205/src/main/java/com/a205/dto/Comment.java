6191e11924b629f5d2048bea4efd0b97ec03e74f
LEESINHO <dltlsgh5@naver.com>
2020-02-04 17:33:08 +0900

    comment_dto

---
+++ back_end/A205/src/main/java/com/a205/dto/Comment.java
@@ -0,0 +1,16 @@
+package com.a205.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Comment {
+	Integer m_id;
+	String c_content;
+	Integer p_id;
+	String c_date;
+}
