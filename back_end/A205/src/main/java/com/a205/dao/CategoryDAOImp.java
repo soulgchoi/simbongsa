75b7df5d9ce4d3eae5d862cd0770bca35862fe45
LEESINHO <dltlsgh5@naver.com>
2020-02-07 13:48:45 +0900

    95% 완성

--- back_end/A205/src/main/java/com/a205/dao/CategoryDAOImp.java
+++ back_end/A205/src/main/java/com/a205/dao/CategoryDAOImp.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.a205.dto.Category;
-import com.a205.dto.Region;
-
+@Repository
 public class CategoryDAOImp implements CategoryDAO{
 	private final static String ns = "com.a205.model.categorymapper.";
 
--- back_end/A205/src/main/java/com/a205/service/MemberServiceImp.java
+++ back_end/A205/src/main/java/com/a205/service/MemberServiceImp.java
@@ -6,15 +6,24 @@ import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.a205.dao.CategoryDAO;
 import com.a205.dao.MemberDAO;
 import com.a205.dao.MemberDAOImp;
+import com.a205.dao.MemberHasCategoryDAO;
 import com.a205.dao.MemberHasRegionDAO;
 import com.a205.dao.RegionDAO;
 import com.a205.dto.Member;
 import com.a205.dto.Region;
 import com.a205.dto.MemberException;
+import com.a205.dto.Member_has_category;
 import com.a205.dto.Member_has_region;
 import com.a205.model.MemberPatchRequest;
 
@@ -29,6 +38,12 @@ public class MemberServiceImp implements MemberService {
 	
 	@Autowired
 	private RegionDAO regionDao;
+	
+	@Autowired
+	private CategoryDAO categoryDao;
+	
+	@Autowired
+	private MemberHasCategoryDAO member_has_category_dao;
 	
 	@Autowired
 	private MemberHasRegionDAO member_has_region_dao;
@@ -73,41 +88,66 @@ public class MemberServiceImp implements MemberService {
 		}
 	}
 	@Override
+	@Transactional
 	public boolean patchUpdate(String userId,MemberPatchRequest memberPatch) {
-		Member member = dao.search(userId);
-		int m_id = member.getM_id();
-		String m_age = memberPatch.getM_age();
-		String m_bgnTm = memberPatch.getM_bgnTm();
-		String m_endTm = memberPatch.getM_endTm();
-
-		// 선호 지역 설정
-		String perfer_region = memberPatch.getPrefer_region();
-		List<String> list = java.util.Arrays.asList(perfer_region.split(" "));
-		for(String sido:list) {
-			List<String> region_bucket = java.util.Arrays.asList(sido.split("-"));
-			Member_has_region  member_has_region = new Member_has_region();
-			member_has_region.setM_id(m_id);
-			// r_sidoCd
-			System.out.println(region_bucket.get(0));
+		try {
+			Member member = dao.search(userId);
+			Integer m_id = member.getM_id();
+			String m_age = memberPatch.getM_age();
+			String m_bgnTm = memberPatch.getM_bgnTm();
+			String m_endTm = memberPatch.getM_endTm();
+			// 선호 지역 설정
+			String perfer_region = memberPatch.getPrefer_region();
+			List<String> list = java.util.Arrays.asList(perfer_region.split(" "));
+			for(String sido:list) {
+				List<String> region_bucket = java.util.Arrays.asList(sido.split("-"));
+				Member_has_region  member_has_region = new Member_has_region();
+				member_has_region.setM_id(m_id);
+				// r_sidoCd
+				System.out.println(region_bucket.get(0));
+				
+				// r_gugunCd
+				System.out.println(region_bucket.get(1));
+				int r_id = regionDao.selectOne(region_bucket.get(0), region_bucket.get(1)).getR_id();
+				member_has_region.setR_id(r_id);
+				boolean member_has_region_created = member_has_region_dao.add(member_has_region);
+				System.out.println(member_has_region_created);
+			}		
 			
-			// r_gugunCd
-			System.out.println(region_bucket.get(1));
-			int r_id = regionDao.selectOne(region_bucket.get(0), region_bucket.get(1)).getR_id();
-			member_has_region.setR_id(r_id);
-			boolean member_has_region_created = member_has_region_dao.add(member_has_region);
-			System.out.println(member_has_region_created);
-		}		
-		
-		String perfer_category = memberPatch.getPrefer_category();
-		member.setM_age(m_age); 
-		member.setM_bgnTm(m_bgnTm);
-		member.setM_endTm(m_endTm);
-		
-//		boolean MemberChangedOk = dao.update(member);
-		
-		
-		return true;
+			String perfer_category = memberPatch.getPrefer_category();
+			List<String> list2 = java.util.Arrays.asList(perfer_category.split(" "));
+			for(String cate:list2) {
+				List<String> cate_bucket = java.util.Arrays.asList(cate.split("-"));
+				Member_has_category member_has_category = new Member_has_category();
+				member_has_category.setM_id(m_id.toString());
+				// r_sidoCd
+				System.out.println(cate_bucket.get(0));
+				
+				// r_gugunCd
+				System.out.println(cate_bucket.get(1));
+				Integer ca_id = categoryDao.selectOne(cate_bucket.get(0),cate_bucket.get(1)).getCa_id();
+				member_has_category.setCa_id(ca_id.toString());
+				boolean member_has_category_created = member_has_category_dao.add(member_has_category);
+				System.out.println(member_has_category_created);
+			}		
+
+			
+			member.setM_age(m_age); 
+			member.setM_bgnTm(m_bgnTm);
+			member.setM_endTm(m_endTm);
+			
+			boolean MemberChangedOk = dao.update(member);
+			
+			
+			return MemberChangedOk;
+
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return false;
 	}
+
 
 	@Override
 	public boolean update(Member member) {
