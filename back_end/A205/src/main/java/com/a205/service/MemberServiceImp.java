dafcfc5dd29d79bd37c3e6c1877c55444692974b
LEESINHO <dltlsgh5@naver.com>
2020-02-10 09:44:55 +0900

    create

--- back_end/A205/src/main/java/com/a205/service/MemberServiceImp.java
+++ back_end/A205/src/main/java/com/a205/service/MemberServiceImp.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
