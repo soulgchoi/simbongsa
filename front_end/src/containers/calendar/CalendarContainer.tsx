efd902ed32b2ff3ffd4d975c9188da7507edb0d4
LimHaksu <neroren2@gmail.com>
2020-02-12 08:50:45 +0900

    merger conflict 해결

--- front_end/src/containers/calendar/CalendarContainer.tsx
+++ front_end/src/containers/calendar/CalendarContainer.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React, { Fragment } from 'react';
-import moment, { Moment as MomentTypes } from "moment";
-
-import { connect } from 'react-redux';
-import { changeDate } from 'redux/modules/calendar';
-import { bindActionCreators } from 'redux';
-import Calendar from 'components/Calendar/Calendar';
-import VolList from 'containers/mainpage/VolList';
-
-=======
 import React from "react";
 import moment, { Moment as MomentTypes } from "moment";
 
@@ -17,28 +6,12 @@ import calendar, { changeDate } from "redux/modules/calendar";
 import { bindActionCreators } from "redux";
 import Calendar from "components/calendar/Calendar";
 import VolList from "../mainpage/VolList";
->>>>>>> 5a5c88a761fbff973ce598349877db8be8f4615a
 interface Props {
   date: MomentTypes;
   changeDate: typeof changeDate;
   toggle: boolean;
 }
 class CalendarContainer extends React.Component<Props> {
-<<<<<<< HEAD
-
-    componentDidMount() {
-        //this.props.changeDate(moment().add(1, 'month'))
-    }
-    render() {
-        const { props } = this;
-        return (
-            <Fragment>
-            <Calendar date={props.date} changeDate={props.changeDate} />
-            <VolList/>
-            </Fragment>
-        )
-    }
-=======
   componentDidMount() {
     //this.props.changeDate(moment().add(1, 'month'))
   }
@@ -57,7 +30,6 @@ class CalendarContainer extends React.Component<Props> {
       </div>
     );
   }
->>>>>>> 5a5c88a761fbff973ce598349877db8be8f4615a
 }
 
 export default connect(
