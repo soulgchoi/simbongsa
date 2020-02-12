04d809e2b144ffcc547fd7667ac1bb2b64a8e00f
Park Jung Hwan <pjh5929@naver.com>
2020-02-12 09:39:17 +0900

    구글 소셜로그인

--- back_end/A205/src/main/java/com/a205/controller/JwtAuthenticationController.java
+++ back_end/A205/src/main/java/com/a205/controller/JwtAuthenticationController.java
@@ -1,5 +1,7 @@
 package com.a205.controller;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.Objects;
 
@@ -23,6 +25,10 @@ import com.a205.model.JwtRequest;
 import com.a205.model.JwtResponse;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 
 @RestController
@@ -43,10 +49,12 @@ public class JwtAuthenticationController {
 
 	@Autowired
 	private MemberDAO memberDao;
-	
+
+	private static final String MY_APP_GOOGLE_CLIENT_ID = "266208955593-716c9b8v63c5vfrure5n1tv8n9lrld6p.apps.googleusercontent.com";
+
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-		
+
 //		//로컬로그인
 //		if(authenticationRequest.getPassword()!=null&&!authenticationRequest.getPassword().isEmpty()) {
 //			authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());			
@@ -57,7 +65,7 @@ public class JwtAuthenticationController {
 //				throw new Exception("Not Available GoogleID");
 //			}
 //		}
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());		
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 		final String email = authenticationRequest.getUsername();
 		final int id = userDetailsService.loadUserIdByUsername(authenticationRequest.getUsername());
 		final String userId = userDetailsService.loadUserNickByUsername(authenticationRequest.getUsername());
@@ -73,32 +81,73 @@ public class JwtAuthenticationController {
 
 	@RequestMapping(value = "/loginByGoogle", method = RequestMethod.POST)
 	public ResponseEntity<?> saveUserG(@RequestBody String receive_idToken) throws Exception {
-		GoogleIdToken idToken = googleTokenUtil.verify(receive_idToken);
-		Member member = new Member();
-		if (idToken != null) {
-			Payload payload = idToken.getPayload();
 
-			// Print user identifier
-			String email = payload.getEmail();
-			String userid = email.substring(0, email.lastIndexOf("@"));
-			System.out.println(userid);
-			member.setM_email(email);
-			member.setM_userid(userid);
-			
-			Member mem = memberDao.searchByEmail(email);
-			if(mem.getM_email().isEmpty()||mem.getM_email()==null) {
-				userDetailsService.saveByGoogle(member);
+		final NetHttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+		final JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+				.setAudience(Collections.singletonList(MY_APP_GOOGLE_CLIENT_ID)).build();
+		GoogleIdToken idToken;
+
+		Member member = new Member();
+		try {
+			idToken = verifier.verify(receive_idToken);
+			System.out.println(idToken);
+			if (idToken == null) {
+				System.out.println("토큰값이 없어요");
 			}
-			member = memberDao.searchByEmail(email);
-			
-		} else {
-			System.out.println("Invalid ID token.");
+			System.out.println(receive_idToken);
+			System.out.println(idToken);
+			if (idToken != null) {
+				Payload payload = idToken.getPayload();
+
+				// Print user identifier
+				String email = payload.getEmail();
+				String userid = email.substring(0, email.lastIndexOf("@"));
+				System.out.println(userid);
+				member.setM_email(email);
+				member.setM_userid(userid);
+
+				Member mem = memberDao.searchByEmail(email);
+				if (mem.getM_email().isEmpty() || mem.getM_email() == null) {
+					userDetailsService.saveByGoogle(member);
+				}
+				member = memberDao.searchByEmail(email);
+			}
+		} catch (GeneralSecurityException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("아니지롱");
 		}
+
+//		GoogleIdToken idToken = googleTokenUtil.verify(receive_idToken);
+//		System.out.println(receive_idToken);
+//		System.out.println(idToken);
+//		Member member = new Member();
+//		if (idToken != null) {
+//			Payload payload = idToken.getPayload();
+//
+//			// Print user identifier
+//			String email = payload.getEmail();
+//			String userid = email.substring(0, email.lastIndexOf("@"));
+//			System.out.println(userid);
+//			member.setM_email(email);
+//			member.setM_userid(userid);
+//			
+//			Member mem = memberDao.searchByEmail(email);
+//			if(mem.getM_email().isEmpty()||mem.getM_email()==null) {
+//				userDetailsService.saveByGoogle(member);
+//			}
+//			member = memberDao.searchByEmail(email);
+//			
+//		} else {
+//			System.out.println("Invalid ID token.");
+//		}
 		final String email = member.getM_email();
 		final int id = member.getM_id();
 		final String userId = member.getM_userid();
 		final String token = jwtTokenUtil.generateToken(email, userId, id);
-		
+
 		return ResponseEntity.ok(new JwtResponse(token));
 	}
 
