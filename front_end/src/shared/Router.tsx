558e69a1affd2bae1e857804afdd873c516c4676
limhaksu <limhaksu@naver.com>
2020-03-02 02:09:54 +0900

    페이지 동적이동 모든 페이지에 적용

--- front_end/src/shared/Router.tsx
+++ front_end/src/shared/Router.tsx
@@ -41,26 +41,9 @@ import FollowingPage from "containers/followpage/FollowingPage"
 import FollowerPage from "containers/followpage/FollowerPage"
 
 
-const filterPathname = (path:string) => {
-  return path.split('/')[3];
-}
-const direction_old = (path: string)=>{
-  let pagepath = filterPathname(path);
-  console.log("여기",pagepath)
-  // 왼쪽으로 이동 : mainpage , feed, mypage, userpage
-  if(pagepath === 'mainpage' || pagepath === 'feed' || pagepath === 'mypage' || pagepath === 'user'){
-    return 'transiton-page-left'
-  }else if(pagepath === 'vol'){
-    return 'transiton-page-left'
-  }else{
-    return 'transiton-page-disappear'
-  }
-}
-
-
-
 class Router extends Component {
   direction = (history : any) => {
+    // POP은 뒤로가기버튼을 눌렀을때 혹은 history.goBack을 호출했을때
     if (history.action === "POP") {
       return 'transiton-page-right'
     }else{
@@ -79,69 +62,61 @@ class Router extends Component {
       <Route path="/" component={HeaderForDesktop} />
       <Route path="/" component={HeaderForMobile} />
       
-      {/* 시작페이지 */}
-      <Route exact path="/" component={Intro} />
-
-      {/* 로그인 */}
-      <Route exact path="/login" component={Login} />
-
-      {/* 회원가입 */}
-      <Route exact path="/join" component={Join} />
-      <Route path="/join/complete" component={JoinComplete} />
-
-      {/* 이메일 인증 */}
-      <Route path="/email/:email/:key" component={EmailComplete} />
-      <Route path="/mailresend" component={MailReSend} />
-
-      {/* 비밀번호찾기 */}
-      <Route path="/findpassword" component={FindPassword} />
-      <Route path="/findpasswordmailsend" component={FindPasswordMailSend} />
-
-      {/* 회원 정보 ooo */}
-      <Route path="/changepassword/:token" component={FindPasswordMailRecieve} />
-      <Route exact path="/usersetting" component={UserSettingPage} />
-      <Route path="/withdraw" component={Withdraw} />
-
-      {/* curation ooo */}
-      
-      <Route path="/calendar" component={CalendarContainer} />
-      <Route exact path="/:id/postinglist" component={PostingList} />
-      <Route exact path="/vol/:id/write" component={PostingForm} />
-
-      {/* 다른회원 정보 ooo */}
-      <Route path="/follower/:id" component={FollowerPage}/>
-      <Route path="/following/:id" component={FollowingPage}/>
-
-      {/* 회원탈퇴 완료 */}
-      <Route path="/withdrawcomplete" component={WithdrawComplete} />
-
-      
-      
-
       {/* 동적으로 화면 전환이 일어나는 페이지 */}
       <Route render={({location, history})=>{
         const currentLocation = window.location.href;
         return(
         <TransitionGroup>
+        {/* classNames 에 들아가는 값에 따라 Router.scss 에서 클래스에 적용한 css가 동작합니다 */}
         <CSSTransition key={location.pathname} classNames={this.direction(history)} timeout={300}>
           <Switch location={location}>
           {/* 이 안에 <Route />를 넣으면 됩니다. */}
           
-          {/* 메인페이지 */}
-          <Route exact path="/mainpage" component={MainPage} />
+          {/* 시작페이지 */}
+          <Route exact path="/" component={Intro} />
 
-          {/* 피드페이지 */}
+          {/* 로그인 */}
+          <Route exact path="/login" component={Login} />
+
+          {/* 회원가입 */}
+          <Route exact path="/join" component={Join} />
+          <Route path="/join/complete" component={JoinComplete} />
+
+          {/* 이메일 인증 */}
+          <Route path="/email/:email/:key" component={EmailComplete} />
+          <Route path="/mailresend" component={MailReSend} />
+
+          {/* 비밀번호찾기 */}
+          <Route path="/findpassword" component={FindPassword} />
+          <Route path="/findpasswordmailsend" component={FindPasswordMailSend} />
+
+          {/* 회원 정보 ooo */}
+          <Route path="/changepassword/:token" component={FindPasswordMailRecieve} />
+          <Route exact path="/usersetting" component={UserSettingPage} />
+          <Route path="/withdraw" component={Withdraw} />
+
+          {/* 메인페이지 ooo */}
+          <Route exact path="/mainpage" component={MainPage} />
+          <Route path="/calendar" component={CalendarContainer} />
+          <Route exact path="/:id/postinglist" component={PostingList} />
+          <Route exact path="/vol/:id/write" component={PostingForm} />
+
+          {/* 피드페이지 ooo */}
           <Route exact path="/feed" component={Feed} />
+
+          {/* 봉사 상세정보 페이지 */}
+          <Route exact path="/vol/:id/detail" component={VolDetail} /> 
 
           {/* 마이페이지 */}
           <Route exact path="/mypage" component={Mypage} />
 
-          {/* 유저페이지 */}
+          {/* 다른회원 정보 ooo */}
           <Route path="/user/:id" component={OthersPage}/>
+          <Route path="/follower/:id" component={FollowerPage}/>
+          <Route path="/following/:id" component={FollowingPage}/>
 
-          {/* 봉사 상세정보 페이지 */}
-          <Route exact path="/vol/:id/detail" component={VolDetail} /> 
-          
+          {/* 회원탈퇴 완료 */}
+          <Route path="/withdrawcomplete" component={WithdrawComplete} />
 
           </Switch>
           </CSSTransition>
