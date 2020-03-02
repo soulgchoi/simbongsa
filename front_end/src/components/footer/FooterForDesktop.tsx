84795285df10f162efbae5c3849951f73c1bb777
min1378 <qwes123@naver.com>
2020-03-02 18:56:37 +0900

    푸터 데스크탑 빼먹은거 수정...

--- front_end/src/components/footer/FooterForDesktop.tsx
+++ front_end/src/components/footer/FooterForDesktop.tsx
@@ -14,12 +14,12 @@ class FooterForDesktop extends React.Component<any, any> {
       <div>
         {/* 큰 화면에서 보여줌 */}
 
-    <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+    <Responsive minWidth={1001}>
     <Segment vertical style={{ padding: '2em 0em', backgroundColor: "rgba(255, 147, 45, 0.61)", marginTop:"30px"}}>
       <Container>
         <Grid divided stackable>
           <Grid.Row>
-            <Grid.Column textAlign="center">
+          <Grid.Column textAlign="center">
               <Header  as='h4' content='About Us' />
               <Container styk>
                 프론트엔드 : 윤성민, 임학수, 최솔지<br/>
